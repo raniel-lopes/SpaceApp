b6892725fd6686e7d0b0f38e901c408aefa7bf80
Raniel Lopes <raniel.ldms@gmail.com>
2024-12-25 12:22:52 -0300

    Criando o header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components"
+import EstilosGlobais from "./Componentes/EstilosGlobais"
+import Cabecalho from "./Componentes/Cabecalho"
 
 const FundoGradiente = styled.div`
   background: var(--Gradiente-fundo, linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%));
@@ -10,7 +12,8 @@ function App() {
 
   return (
     <FundoGradiente>
-
+      <EstilosGlobais />
+      <Cabecalho />
     </FundoGradiente>
   )
 }
