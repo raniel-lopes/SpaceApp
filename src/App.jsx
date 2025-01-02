d9f1ec78842b479b3af83614e02444a48f4c0dec
Raniel Lopes <raniel.ldms@gmail.com>
2025-01-02 16:50:40 -0300

    formatando a barra de navegação e criando o banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components"
 import EstilosGlobais from "./Componentes/EstilosGlobais"
 import Cabecalho from "./Componentes/Cabecalho"
 import BarraLateral from "./Componentes/BarraLateral"
+import Banner from "./Componentes/Banner"
+import bannerBackground from './assets/banner.png'
 
 const FundoGradiente = styled.div`
   background: var(--Gradiente-fundo, linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%));
@@ -16,6 +18,10 @@ function App() {
       <EstilosGlobais />
       <Cabecalho />
       <BarraLateral />
+      <Banner
+        texto="A galeria mais completa de fotos do espaço!"
+        backgroundImage={bannerBackground}
+      />
     </FundoGradiente>
   )
 }
