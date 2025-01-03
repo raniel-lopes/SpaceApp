b697e78e15101ef1449ed72eeb6d9666542b7981
Raniel Lopes <raniel.ldms@gmail.com>
2025-01-02 22:13:33 -0300

    Estilizando o ModalZoom e os Populares da Galeria

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import BarraLateral from "./Componentes/BarraLateral"
 import Banner from "./Componentes/Banner"
 import bannerBackground from './assets/banner.png'
 import Galeria from "./Componentes/Galeria"
-
 import fotos from './fotos.json'
 import { useState } from "react"
 import ModalZoom from "./Componentes/ModalZoom"
@@ -33,6 +32,8 @@ const ConteudoGaleria = styled.section`
 
 const App = () => {
   const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
+  const [fotoSelecionada, setFotoSelecionada] = useState(null)
+
   return (
     <FundoGradiente>
       <EstilosGlobais />
@@ -45,11 +46,17 @@ const App = () => {
               texto="A galeria mais completa de fotos do espaço!"
               backgroundImage={bannerBackground}
             />
-            <Galeria fotos={fotosDaGaleria} />
+            <Galeria
+              aoFotoSelecionada={foto => setFotoSelecionada(foto)}
+              fotos={fotosDaGaleria}
+            />
           </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
-      <ModalZoom />
+      <ModalZoom
+        foto={fotoSelecionada}
+        aoFechar={() => setFotoSelecionada(null)}
+      />
     </FundoGradiente>
   )
 }
